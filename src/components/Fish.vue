32bf7ccedb94e397f49aa04dabff2822f90a10cd
campbell-frost <campbellsfrost@gmail.com>
2024-02-24 00:20:56 -0500

    changed fish location

--- src/components/Fish.vue
+++ src/components/Fish.vue
@@ -75,7 +75,7 @@ export default {
     },
     computed: {
         currentFishUrl() {
-            return (`/Fish/Fish${this.currentFishIndex}.jpg`);
+            return (`./Fish/Fish${this.currentFishIndex}.jpg`);
         }
     },
     methods: {
